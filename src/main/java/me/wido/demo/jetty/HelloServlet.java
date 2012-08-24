f69c5602560fc7cabcc2f233b8bda8a3bb4a5270
sunteya <sunteya@gmail.com>
2012-08-24 08:47:14 +0800

    Use servlet 3.0 API

--- src/main/java/me/wido/demo/jetty/HelloServlet.java
+++ src/main/java/me/wido/demo/jetty/HelloServlet.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1111682915889034251L;
